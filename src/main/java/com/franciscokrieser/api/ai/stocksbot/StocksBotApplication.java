3378cabbeaa7407cf66c9fe3e9b3a674f31462c1
franciscok <francisco.krieser@mttnow.com>
2017-04-15 13:19:31 -0300

    first commit

---
+++ src/main/java/com/franciscokrieser/api/ai/stocksbot/StocksBotApplication.java
@@ -0,0 +1,12 @@
+package com.franciscokrieser.api.ai.stocksbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/franciscokrieser/api/ai/stocksbot/StocksBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.franciscokrieser.api.ai.stocksbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StocksBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
